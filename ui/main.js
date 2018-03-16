99b6147b59dbc1e55c9e1ac705de9fc4ff9714e5
sachinprajapati007 <36616502+sachinprajapati007@users.noreply.github.com>
2018-03-16 23:44:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ button.onclick = function(){
         }
     };
     
-    request.open('GET','http://coco98.imad.hasura-app.io/counter',true);
+    request.open('GET','http://sprajapati751996.imad.hasura-app.io/counter',true);
     request.send(null);
     
 };
